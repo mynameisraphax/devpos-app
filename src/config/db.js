9eedf0910f82e1e20a030d111ac1fe10d851ed13
Rafael Pereira <mynameisraphax@gmail.com>
2025-05-11 19:02:28 -0300

    Atualizando db.js para credenciais da VPS

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'devpos_db'
+  user: 'devpos_user',
+  password: 'senha123',
+  database: 'devpos_db',
 });
 module.exports = pool;
